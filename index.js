df97b299754fa88e8afdc9e9da6005d27bdd1f3d
Sabiha Bary <sabihaab9@gmail.com>
2022-04-20 01:23:40 -0400

    commit tree 7d8d799e2a59ed8bfa48c35769bc0bfd28a3debb

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Your code here
+function moveDodgerLeft(){
+    const dodger = document.getElementById("dodger");
+    dodger.style.backgroundColor = "#FF69B4";
+    dodger.style.bottom = "100px";
+    dodger.style.bottom = "0px";
+    dodger.style.left = "0px";
+}
+
+function moveDodgerRight(){
+    const dodger = document.getElementById("dodger");
+    dodger.style.backgroundColor = "#FF69B4";
+    dodger.style.bottom = "100px";
+    dodger.style.bottom = "0px";
+    dodger.style.left = "100px";
+}
+
